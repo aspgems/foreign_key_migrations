mlomnicki/automatic_foreign_key

allow to disable automatic foreign keys creations
use config.disable = true in initializer

lib/automatic_foreign_key/active_record/connection_adapters/table_definition.rb
@@ -20,16 +20,18 @@ module AutomaticForeignKey::ActiveRecord::ConnectionAdapters
 
     def column_with_automatic_foreign_key(name, type, options = {})
       column_without_automatic_foreign_key(name, type, options)
-      references = ActiveRecord::Base.references(self.name, name, options)
-      if references
-        AutomaticForeignKey.set_default_update_and_delete_actions!(options)
-        foreign_key(name, references.first, references.last, options) 
-        if index = afk_index_options(options)
-          # append [column_name, index_options] pair
-          self.indices << [name, AutomaticForeignKey.options_for_index(index)]
+      unless AutomaticForeignKey.disable
+        references = ActiveRecord::Base.references(self.name, name, options)
+        if references
+          AutomaticForeignKey.set_default_update_and_delete_actions!(options)
+          foreign_key(name, references.first, references.last, options) 
+          if index = afk_index_options(options)
+            # append [column_name, index_options] pair
+            self.indices << [name, AutomaticForeignKey.options_for_index(index)]
+          end
+        elsif options[:index]
+          self.indices << [name, AutomaticForeignKey.options_for_index(options[:index])]
         end
-      elsif options[:index]
-        self.indices << [name, AutomaticForeignKey.options_for_index(options[:index])]
       end
       self
     end

lib/automatic_foreign_key/active_record/migration.rb
@@ -29,13 +29,17 @@ module AutomaticForeignKey::ActiveRecord
       #
       def add_column(table_name, column_name, type, options = {})
         super
-        handle_column_options(table_name, column_name, options)
+        unless AutomaticForeignKey.disable
+          handle_column_options(table_name, column_name, options)
+        end
       end
 
       def change_column(table_name, column_name, type, options = {})
         super
-        remove_foreign_key_if_exists(table_name, column_name)
-        handle_column_options(table_name, column_name, options)
+        unless AutomaticForeignKey.disable
+          remove_foreign_key_if_exists(table_name, column_name)
+          handle_column_options(table_name, column_name, options)
+        end
       end
 
       protected

spec/migration_spec.rb
@@ -54,7 +54,7 @@ describe ActiveRecord::Migration do
                    :state => { :index => {:with => :city} } ) 
       @model.should have_index.on([:state, :city])
     end
-    
+
     it "should auto-index foreign keys only" do
       AutomaticForeignKey.auto_index = true
       create_table(@model,  :user_id => {},
@@ -182,17 +182,6 @@ describe ActiveRecord::Migration do
       AutomaticForeignKey.on_delete = nil
     end
 
-    protected
-    def add_column(column_name, *args)
-      table = @model.table_name
-      ActiveRecord::Migration.suppress_messages do
-        ActiveRecord::Migration.add_column(table, column_name, *args)
-        @model.reset_column_information
-        yield if block_given?
-        ActiveRecord::Migration.remove_column(table, column_name)
-      end
-    end
-
   end
 
   context "when column is changed" do
@@ -225,18 +214,40 @@ describe ActiveRecord::Migration do
       end
 
     end
-    
-    protected
-    def change_column(column_name, *args)
-      table = @model.table_name
-      ActiveRecord::Migration.suppress_messages do
-        ActiveRecord::Migration.change_column(table, column_name, *args)
-        @model.reset_column_information
+
+    context "when disabled" do
+
+      before do
+        @model = Post
       end
+
+      around do |example|
+        disable(&example)
+      end
+
+      it "should not create foreign key for created table" do
+        create_table(@model, :user_id => {})
+        @model.should_not reference.on(:user_id)
+      end
+
+      it "should not create foreign for added column" do
+        create_table(@model, {})
+        add_column(:user_id, :integer) do
+          @model.should_not reference.on(:user_id)
+        end
+      end
+
+      it "should not create foreign key for changed column" do
+        create_table(@model, :user_id => { :references => nil })
+        change_column(:user_id, :integer)
+        @model.should_not reference.on(:user_id)
+      end
+
     end
 
   end
-    
+
+  protected
   def foreign_key(model, column)
     columns = Array(column).collect(&:to_s)
     model.foreign_keys.detect { |fk| fk.table_name == model.table_name && fk.column_names == columns } 
@@ -250,6 +261,34 @@ describe ActiveRecord::Migration do
         end
       end
       model.reset_column_information
+    end
+  end
+
+  def add_column(column_name, *args)
+    table = @model.table_name
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Migration.add_column(table, column_name, *args)
+      @model.reset_column_information
+      yield if block_given?
+      ActiveRecord::Migration.remove_column(table, column_name)
+    end
+  end
+
+  def change_column(column_name, *args)
+    table = @model.table_name
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Migration.change_column(table, column_name, *args)
+      @model.reset_column_information
+    end
+  end
+
+  def disable(&block)
+    old_value = AutomaticForeignKey.disable
+    AutomaticForeignKey.disable = true
+    begin
+      yield
+    ensure
+      AutomaticForeignKey.disable = old_value
     end
   end
 

aspgems/foreign_key_migrations

lib/foreign_key_migrations/active_record/connection_adapters/table_definition.rb
@@ -17,16 +17,18 @@ module ForeignKeyMigrations::ActiveRecord::ConnectionAdapters
 
     def column_with_foreign_key_migrations(name, type, options = {})
       column_without_foreign_key_migrations(name, type, options)
-      references = ActiveRecord::Base.references(self.name, name, options)
-      if references
-        ForeignKeyMigrations.set_default_update_and_delete_actions!(options)
-        foreign_key(name, references.first, references.last, options)
-        if index = fkm_index_options(options)
-          # append [column_name, index_options] pair
-          self.indices << [name, ForeignKeyMigrations.options_for_index(index)]
+      unless ForeignKeyMigrations.disable
+        references = ActiveRecord::Base.references(self.name, name, options)
+        if references
+          ForeignKeyMigrations.set_default_update_and_delete_actions!(options)
+          foreign_key(name, references.first, references.last, options)
+          if index = fkm_index_options(options)
+            # append [column_name, index_options] pair
+            self.indices << [name, ForeignKeyMigrations.options_for_index(index)]
+          end
+        elsif options[:index]
+          self.indices << [name, ForeignKeyMigrations.options_for_index(options[:index])]
         end
-      elsif options[:index]
-        self.indices << [name, ForeignKeyMigrations.options_for_index(options[:index])]
       end
       self
     end

lib/foreign_key_migrations/active_record/migration.rb
@@ -29,13 +29,17 @@ module ForeignKeyMigrations::ActiveRecord
       #
       def add_column(table_name, column_name, type, options = {})
         super
-        handle_column_options(table_name, column_name, options)
+        unless ForeignKeyMigrations.disable
+          handle_column_options(table_name, column_name, options)
+        end
       end
 
       def change_column(table_name, column_name, type, options = {})
         super
-        remove_foreign_key_if_exists(table_name, column_name)
-        handle_column_options(table_name, column_name, options)
+        unless ForeignKeyMigrations.disable
+          remove_foreign_key_if_exists(table_name, column_name)
+          handle_column_options(table_name, column_name, options)
+        end
       end
 
       protected

spec/migration_spec.rb
@@ -54,7 +54,7 @@ describe ActiveRecord::Migration do
                    :state => { :index => {:with => :city} } ) 
       @model.should have_index.on([:state, :city])
     end
-    
+
     it "should auto-index foreign keys only" do
       ForeignKeyMigrations.auto_index = true
       create_table(@model,  :user_id => {},
@@ -182,17 +182,6 @@ describe ActiveRecord::Migration do
       ForeignKeyMigrations.on_delete = nil
     end
 
-    protected
-    def add_column(column_name, *args)
-      table = @model.table_name
-      ActiveRecord::Migration.suppress_messages do
-        ActiveRecord::Migration.add_column(table, column_name, *args)
-        @model.reset_column_information
-        yield if block_given?
-        ActiveRecord::Migration.remove_column(table, column_name)
-      end
-    end
-
   end
 
   context "when column is changed" do
@@ -225,18 +214,40 @@ describe ActiveRecord::Migration do
       end
 
     end
-    
-    protected
-    def change_column(column_name, *args)
-      table = @model.table_name
-      ActiveRecord::Migration.suppress_messages do
-        ActiveRecord::Migration.change_column(table, column_name, *args)
-        @model.reset_column_information
+
+    context "when disabled" do
+
+      before do
+        @model = Post
       end
+
+      around do |example|
+        disable(&example)
+      end
+
+      it "should not create foreign key for created table" do
+        create_table(@model, :user_id => {})
+        @model.should_not reference.on(:user_id)
+      end
+
+      it "should not create foreign for added column" do
+        create_table(@model, {})
+        add_column(:user_id, :integer) do
+          @model.should_not reference.on(:user_id)
+        end
+      end
+
+      it "should not create foreign key for changed column" do
+        create_table(@model, :user_id => { :references => nil })
+        change_column(:user_id, :integer)
+        @model.should_not reference.on(:user_id)
+      end
+
     end
 
   end
-    
+
+  protected
   def foreign_key(model, column)
     columns = Array(column).collect(&:to_s)
     model.foreign_keys.detect { |fk| fk.table_name == model.table_name && fk.column_names == columns } 
@@ -250,6 +261,34 @@ describe ActiveRecord::Migration do
         end
       end
       model.reset_column_information
+    end
+  end
+
+  def add_column(column_name, *args)
+    table = @model.table_name
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Migration.add_column(table, column_name, *args)
+      @model.reset_column_information
+      yield if block_given?
+      ActiveRecord::Migration.remove_column(table, column_name)
+    end
+  end
+
+  def change_column(column_name, *args)
+    table = @model.table_name
+    ActiveRecord::Migration.suppress_messages do
+      ActiveRecord::Migration.change_column(table, column_name, *args)
+      @model.reset_column_information
+    end
+  end
+
+  def disable(&block)
+    old_value = ForeignKeyMigrations.disable
+    ForeignKeyMigrations.disable = true
+    begin
+      yield
+    ensure
+      ForeignKeyMigrations.disable = old_value
     end
   end
 